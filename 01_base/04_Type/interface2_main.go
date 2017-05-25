e38264f071ff5943c1a4abd02a31c8135615c18e
zhuzhenyu <zhuzhenyu307@gmail.com>
2017-05-26 02:05:04 +0800

    Fix interface2 comment

--- 01_base/04_Type/interface2_main.go
+++ 01_base/04_Type/interface2_main.go
@@ -10,12 +10,23 @@ import (
 
 func main() {
 	var it interface{}
-	it = 10  //不会报错 reflect.TypeOf(it)  int
-	it = "a" //不会报错 reflect.TypeOf(it)  string
+	it = 10 //不会报错 reflect.TypeOf(it)  int
 	it = 10
 	var it2 int
 	it2 = 10
 	_ = it2
-	fmt.Println("it typeof ", reflect.TypeOf(it))
-	fmt.Println("it == i2  true or false ?  ", it == it2)
+	fmt.Println("it typeof ", reflect.TypeOf(it))         //int
+	fmt.Println("it == i2  true or false ?  ", it == it2) // true
+	it = "a"                                              //不会报错 reflect.TypeOf(it)  string
+	it3 := "a"
+	_ = it3
+	fmt.Println("it typeof ", reflect.TypeOf(it))         //string
+	fmt.Println("it == i2  true or false ?  ", it == it3) // true
 }
+
+//$ go run interface2_main.go
+
+//it typeof  int
+//it == i2  true or false ?   true
+//it typeof  string
+//it == i2  true or false ?   true
