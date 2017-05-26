482dd8e9a5a33ab5a01aad36fdf30e22ff0dc41f
zhuzhenyu <zhuzhenyu307@gmail.com>
2017-05-27 00:33:08 +0800

    Add simple defer

---
+++ 01_base/05_Func/defer.go
@@ -0,0 +1,21 @@
+/**
+ *  datetime: 2017-05-27
+ *  defer 延迟调用。通常用于资源释放和错误处理
+ */
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("before")
+	defer fmt.Println("defer1")
+	defer fmt.Println("defer2")
+	fmt.Println("after")
+	//以上执行结果 - 当有多个defer时， 按照FILO次序进行(First in Last out) 顺序栈方式
+	// before
+	// after
+	// defer
+}
