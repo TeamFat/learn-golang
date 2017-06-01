a006674aee3e0551fb08417025a2a852afdd12ce
zhuzhenyu <zhuzhenyu307@gmail.com>
2017-06-01 23:51:25 +0800

    Add select example

---
+++ 02_goroutine/03_Select/select2.go
@@ -0,0 +1,32 @@
+/**
+ * datetime: 2017-06-01
+ * select example
+ */
+package main
+
+import "fmt"
+
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
+func main() {
+	ch := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-ch)
+		}
+		quit <- 0
+	}()
+	fibonacci(ch, quit)
+}
