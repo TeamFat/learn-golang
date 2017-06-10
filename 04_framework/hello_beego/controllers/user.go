073c97e846220c3cea0d2ffe7160f5c4f1379966
zhuzhenyu <zhuzhenyu307@gmail.com>
2017-06-10 23:43:03 +0800

    beego data bind

--- 04_framework/hello_beego/controllers/user.go
+++ 04_framework/hello_beego/controllers/user.go
@@ -1,17 +1,15 @@
 package controllers
 
-import (
-	"strconv"
-)
-
 type UserController struct {
 	BaseController
 }
 
 func (this *UserController) Get() {
 	userId := this.Ctx.Input.Param(":id")
-	intUserId, _ := strconv.ParseInt(userId, 10, 64)
-	this.Data["name"] = "jsser"
-	this.Data["user_id"] = intUserId
+	_ = userId
+	//支持数据绑定 - 将数据直接绑定到id上
+	var id int
+	this.Ctx.Input.Bind(&id, ":id")
+	this.Data["json"] = map[string]interface{}{"user_id": id, "username": "jsser"}
 	this.ServeJSON()
 }
