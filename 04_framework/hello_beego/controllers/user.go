3bdf3c88da9db4ea1867095a9291ed8c9a14564b
zhuzhenyu <zhuzhenyu307@gmail.com>
2017-06-10 23:51:51 +0800

    Add getParam methods

--- 04_framework/hello_beego/controllers/user.go
+++ 04_framework/hello_beego/controllers/user.go
@@ -5,8 +5,13 @@ type UserController struct {
 }
 
 func (this *UserController) Get() {
+	//方式1
 	userId := this.Ctx.Input.Param(":id")
 	_ = userId
+	//方式2 - 直接获取的是int类型 , 如果获取其他类型： GetString(), GetStrings(), GetInt(), GetBool, GetFloat()
+	userId2 := this.Input().Get("id")
+	_ = userId2
+	//方式3
 	//支持数据绑定 - 将数据直接绑定到id上
 	var id int
 	this.Ctx.Input.Bind(&id, ":id")
